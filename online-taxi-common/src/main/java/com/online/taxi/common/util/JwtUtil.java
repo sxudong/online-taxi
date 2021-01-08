fe1b0bb0ddc8f59b411333a454e7167d5b8d66dd
sxudong <15295901@qq.com>
2021-01-08 23:19:32 +0800

    update

--- online-taxi-common/src/main/java/com/online/taxi/common/util/JwtUtil.java
+++ online-taxi-common/src/main/java/com/online/taxi/common/util/JwtUtil.java
@@ -16,24 +16,29 @@ public class JwtUtil {
     private static String secret = "ko346134h_we]rg3in_yip1!";
 
     /**
-     *
      * @param subject
      * @param issueDate 签发时间
      * @return
      */
     public static String createToken(String subject, Date issueDate) {
+        // setIssuer()     jwt签发者
+        // setSubject()    jwt所面向的用户
+        // setIssuedAt()   jwt的签发时间
+        // setExpiration() jwt的过期时间
+        // setNotBefore()  定义在什么时间之前，该jwt都是不可用的
+        // setAudience()   接收jwt的一方
         String compactJws = Jwts.builder()
-                .setSubject(subject)
-                .setIssuedAt(issueDate)
-                .setExpiration(new Date(System.currentTimeMillis()+10000))
+                .setSubject(subject)     //Payload有效载荷
+                .setIssuedAt(issueDate)  //Payload有效载荷
+                .setExpiration(new Date(System.currentTimeMillis() + 10000))
                 .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secret)
                 .compact();
         return compactJws;
-
     }
 
     /**
      * 解密 jwt
+     *
      * @param token
      * @return
      * @throws Exception
@@ -41,30 +46,27 @@ public class JwtUtil {
     public static String parseToken(String token) {
         try {
             Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-            if (claims != null){
+            if (claims != null) {
                 return claims.getSubject();
             }
-        }catch (ExpiredJwtException e){
+        } catch (ExpiredJwtException e) {
             e.printStackTrace();
             System.out.println("jwt过期了");
         }
-
         return "";
     }
 
     public static void main(String[] args) {
         String subject = "wo";
-        String token = createToken(subject,new Date());
+        String token = createToken(subject, new Date());
         System.out.println(token);
         try {
-//			Thread.sleep(10010);
-        	Thread.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        System.out.println("原始值："+parseToken(token));
-
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("原始值：" + parseToken(token));
     }
 
 }
